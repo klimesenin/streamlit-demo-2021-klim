83cf45f7d667966860aa3c4f2245549e0b8fbf43
klimesenin <kesenin@nes.ru>
2021-04-11 20:24:58 +0300

    initial commit

--- demo_app.py
+++ demo_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import streamlit as st
 import numpy as np
 import plotly.graph_objs as go
-
+import seaborn as sns
 
 with st.echo(code_location='below'):
 
@@ -33,16 +33,13 @@ with st.echo(code_location='below'):
 
     a = np.array(toptitle)
     b = np.array(toprate)
+    ###FROM: https://github.com/hupili/python-for-data-and-media-communication-gitbook/issues/118
     c = list(plt.cm.colors.cnames.keys())[10:20]
+    ###END FROM
 
-    ### FROM:
-    #n = toptitle.__len__() + 1
-    #all_colors = list(plt.cm.colors.cnames.keys())
-    #c = random.choices(all_colors, k=n)
-    ### END FROM
     st.title("Рейтинг топ 10 фильмов в зависимости от года выпуска.")
     st.write("Здесь вы можете выбрать год выпуска фильма в период от 1946 года до 2021(все имеющиеся актуальные данные на Netflix) и узнать наилучшие фильмы имеющиеся на "
-             "этом сервисе.")
+             "этом сервисе.(лучше выбирать года попозже, а то старых фильмов там мало")
     fig, ax = plt.subplots()
     ax.bar(a, b, color=c, width=0.5)
     for item in ([ax.title, ax.xaxis.label] +
@@ -52,7 +49,7 @@ with st.echo(code_location='below'):
 
     pog['rawdur'] = pd.to_numeric(pog['duration'].apply(lambda x: x.split(" ", 1)[0]))
 
-    # спизжено
+    ###FROM: https://www.youtube.com/watch?v=dwBkTbyHWY4
     data = [go.Scatter(
         x=pog['release_year'],
         y=pog['weighted_average_vote'],
@@ -66,18 +63,18 @@ with st.echo(code_location='below'):
         yaxis=dict(title='Рейтинг'),
         hovermode='closest'
     )
-
+    ###END FROM
+    st.write("Перейдем к следующему графику.Во первых, чтобы было понятнее, размер шарика на графике показывает его длительность, распределены шарики в соотсветствии со своим годом выпуска и рейтингом. Чтобы узнать, конкретный период или инетерсующий вас разброс"
+        " рейтинга, можно выделить область на графике и посмотреть в приближенном варианте." )
     fug = go.Figure(data=data, layout=layout)
     st.plotly_chart(fug)
     pog_indian = pog[pog["country"] == 'India']
     pog_world = pog[pog["country"] != 'India']
-    st.write(
-        "Данный график показывает множество интересных зависимостей. Во первых, чтобы было понятнее, размер шарика на графике показывает его длительность, распределены шарики в соотсветствии со своим годом выпуска и рейтингом. Чтобы узнать, конкретный период или инетерсующий вас разброс"
-        "рейтинга, можно выделить область на графике и посмотреть в приближенном варианте."
-        "Можно заметить, что большинство фильмов сосредоточено ближе к текущему году, и старые фильмы с низким рейтингом также в большинстве своем отсутствуют. Это говорит о том, что политика Netflix ориентирована на современную молодежь"
-        "Также, можно заметить, что в среднем длительность фильмов упала к текущему году, это говорит о более высокой клиповости мышления у современной аудитории")
-    #
-    st.title("Жанры и актерыheroku create your-app-name --buildpack heroku/python")
+    st.write("По этому графику можем выделить несколько зависимостей")
+    st.write("1) Можно заметить, что большинство фильмов сосредоточено ближе к текущему году, и старые фильмы с низким рейтингом также в большинстве своем отсутствуют. Это говорит о том, что политика Netflix ориентирована на современную молодежь")
+    st.write("2) Также, можно заметить, что в среднем длительность фильмов упала к текущему году, это говорит о более высокой клиповости мышления у современной аудитории")
+
+    st.title("Жанры и актеры")
     st.write(
         "В дальнейших данных я решил разделить индийский кинематограф и кинематограф остального мира для получения более адеквавтной выборки для целевой аудитории данного дэшборда"
         ", но если есть фанаты Индийского кино, то наслаждайтесь")
@@ -177,8 +174,13 @@ with st.echo(code_location='below'):
         st.pyplot(fig1)
     except NameError:
         pass
-
-
+    st.title("Для тех, кому интересно какие актеры сыграли в наибольшем количестве фильмов на Netflix можете глянуть, они также разделены на индусов и остальнйо мир")
+    sns.set(font_scale=1)
+    f2, ax = plt.subplots(figsize=(10, 30))
+    colors_cw = sns.color_palette('magma', len(pogchamp['Times in movie'].tolist()))
+    sns.barplot(pogchamp.index, pogchamp['Times in movie'], palette=colors_cw[::-1])
+    Text = ax.set(xlabel='nya ichi ni san', title='nya arigato')
+    st.pyplot(f2)
     if st.checkbox('Данные по жанрам'):
         st.write(datagenre)
     if st.checkbox("Данные по актерам"):
